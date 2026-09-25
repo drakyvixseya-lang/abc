266133004f73d44ec9cfea0bb86a8373f5d38523
drakyvixseya-lang <drakyvixseya@gmail.com>
2026-09-25 23:32:24 +0530

    Fix module.exports formatting in settings.js

--- settings.js
+++ settings.js
@@ -3,9 +3,9 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-module.exports = {+94772167124
+module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=d26214cda603a92f' : process.env.SESSION_ID,𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=d26214cda603a92f
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=d26214cda603a92f' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
